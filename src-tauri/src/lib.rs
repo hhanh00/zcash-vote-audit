e649c4abee0e14867a55e369df29cc026df2e6e9
Hanh <hanh425@gmail.com>
2025-01-04 16:17:34 +1000

    feat: audit form

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,13 @@
+#[tauri::command]
+async fn audit(url: String, seed: String) -> Result<String, String> {
+    Ok("OK".to_string())
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![])
+        .invoke_handler(tauri::generate_handler![audit])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,64 @@
+import { Button, Card, Label, TextInput } from "flowbite-react";
 import "./App.css";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import { invoke } from "@tauri-apps/api/core";
+
+type AuditParams = {
+  url: string;
+  seed: string;
+}
 
 function App() {
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      url: '',
+      seed: '',
+    },
+  })
+
+  const onSubmit: SubmitHandler<AuditParams> = (params) => {
+    console.log(params);
+
+    (async () => {
+      const res = await invoke('audit', params)
+      console.log(res)
+    })()
+  }
+
   return (
     <main>
+      <form className="flex justify-center items-center h-screen bg-gray-100" onSubmit={handleSubmit(onSubmit)}>
+        <Card className="w-full max-w-md">
+          <h2 className="text-2xl font-bold mb-4 text-center">Election Audit</h2>
+          <div>
+            <Label htmlFor="url" value="Election URL" />
+            <Controller
+              name="url"
+              control={control}
+              render={({ field }) => <TextInput
+                id="url"
+                type="text"
+                required
+                {...field}
+              />} />
+          </div>
+
+          <div>
+            <Label htmlFor="seed" value="Seed Phrase" />
+            <Controller
+              name="seed"
+              control={control}
+              render={({ field }) =>
+                <TextInput
+                  id="seed"
+                  required
+                  {...field}
+                />} />
+          </div>
+
+          <Button type="submit">Verify Ballots and Show Results</Button>
+        </Card>
+      </form>
     </main>
   );
 }
