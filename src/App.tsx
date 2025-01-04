05361b96a5f2dd81af9822567a5e4784b57304f1
Hanh <hanh425@gmail.com>
2025-01-05 01:05:38 +1000

    feat: check ballots & report results

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,25 @@ import { Button, Card, Label, TextInput } from "flowbite-react";
 import "./App.css";
 import { Controller, SubmitHandler, useForm } from "react-hook-form";
 import { invoke } from "@tauri-apps/api/core";
+import { EChart } from "@kbox-labs/react-echarts";
+import { useState } from "react";
 
 type AuditParams = {
   url: string;
   seed: string;
 }
 
+type Count = {
+  choice: string;
+  amount: number;
+}
+
 function App() {
+  const [results, setResults] = useState<Count[]>([])
   const { control, handleSubmit } = useForm({
     defaultValues: {
-      url: '',
-      seed: '',
+      url: 'http://localhost:8000/election/nba-finals',
+      seed: 'broom ugly valve manual relief basket skill brush outer basic museum catalog fun monitor almost kick easy seek myth token jeans slogan clean eternal',
     },
   })
 
@@ -20,14 +28,18 @@ function App() {
     console.log(params);
 
     (async () => {
-      const res = await invoke('audit', params)
+      const res: Count[] = await invoke('audit', params)
       console.log(res)
+      setResults(res)
     })()
   }
 
+  const labels = results.map((c) => c.choice)
+  const votes = results.map((c) => c.amount / 100000)
+
   return (
     <main>
-      <form className="flex justify-center items-center h-screen bg-gray-100" onSubmit={handleSubmit(onSubmit)}>
+      {results.length == 0 && <form className="flex justify-center items-center bg-gray-100" onSubmit={handleSubmit(onSubmit)}>
         <Card className="w-full max-w-md">
           <h2 className="text-2xl font-bold mb-4 text-center">Election Audit</h2>
           <div>
@@ -58,7 +70,35 @@ function App() {
 
           <Button type="submit">Verify Ballots and Show Results</Button>
         </Card>
-      </form>
+      </form>}
+      <EChart
+          renderer={'svg'}
+          onClick={() => console.log('clicked!')}
+          style={{
+            height: '600px',
+            width: '100%'
+          }}
+          xAxis={{
+            type: 'category',
+            data: labels
+          }}
+          yAxis={{
+            type: 'value'
+          }}
+          series={[
+            {
+              data: votes,
+              type: 'bar',
+              showBackground: true,
+              backgroundStyle: {
+                color: 'rgba(180, 180, 180, 0.2)'
+              },
+              label: {
+                show: true,
+                position: 'inside'
+              }            }
+          ]}
+        />
     </main>
   );
 }
